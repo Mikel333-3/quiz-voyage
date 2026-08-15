c32eb53d3a0e14fa3e4d5fd93a35754dabd74444
Mikel333-3 <pierremischaelw@gmail.com>
2026-08-15 19:27:10 +0200

    Refresh PWA cache after audio fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "quiztime-go-v2";
+const CACHE_NAME = "quiztime-go-v3";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./quiztime-favicon.svg"];
 
 self.addEventListener("install", (event) => {
